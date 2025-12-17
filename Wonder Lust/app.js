5f6c2f1a32022fa60193092f64dc0fc96db554f3
Shivam Verma <inocentshivam2002@gmail.com>
2025-12-17 19:29:59 +0530

    Added authentication, signup flow, and passport setup

--- Wonder Lust/app.js
+++ Wonder Lust/app.js
@@ -5,6 +5,20 @@ const Listing = require( "./models/listing.js")
 const path= require ( "path");
 const methodOverride= require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync= require( "./utils/wrapAsync.js");
+const ExpressError= require( "./utils/ExpressError.js");
+const { listingSchema,reviewSchema } = require( "./schema.js");
+const Review = require( "./models/review.js");
+const listings = require( "./routes/listing.js");
+const reviews = require( "./routes/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport= require("passport");
+const LocalStrategy= require("passport-local");
+const User= require( "./models/user.js");
+const listingRouter= require( "./routes/listing.js");
+const reviewRouter= require( "./routes/review.js");
+const userRouter= require( "./routes/user.js");
 
 
 
@@ -26,74 +40,76 @@ app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname ,"/public")));
 
 
-    
+const sessionOptions= {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    },
+}; 
+
+
 
 
 app.get("/", (req,res)=>{
     res.send( " hi i am root ");
 });
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs")
-});
 
-app.post ("/listings", async (req,res)=>{
-    const newListing= new Listing (req.body.listing);
-    await newListing.save();
-    res.redirect("/listings")
+
+
+app.use( session(sessionOptions));
+app.use( flash());
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
     
+app.use((req,res,next)=>{
+    res.locals.success= req.flash("success");
+    res.locals.error= req.flash("error");
+    next();
 });
 
-
-app.get("/listings", async (req,res)=>{
-   const allListings= await Listing.find({});
-   res.render("listings/index.ejs",{allListings});
-     
-
-  
-});
-
-app.get("/listings/:id",async(req,res)=>{
-    let{id}= req.params;
-    const listing= await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-});
-
-
-// app.get("/testListing",  async(req,res)=>{
-//     let sampleListing= new Listing({
-//         title: "My new villa",
-//         description: " by the beach ",
-//         price: 21000,
-//         location: " calangute , goa ",
-//         country : " India",
+// app.get("/demouser", async(req,res)=>{
+//     let fakeUser= new User({
+//         username: "shivam",
+//         email: "student@gmail.com"
 //     });
-//     await sampleListing.save();
-//     console.log( "sample was saved");
-//     res.send( " sucessful testing");
-
-// edit route 
-app.get("/listings/:id/edit",async(req,res)=>{
-        let{id}= req.params;
-        const listing= await Listing.findById(id);
-        res.render("listings/edit.ejs", {listing});
+    
+//     let registeredUser = await User.register(fakeUser, "hello123");
+//     res.send(registeredUser); 
+// });    
 
 
-});
-app.put("/listings/:id",async(req,res)=>{
-    let{id}= req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    res.redirect(`/listings/${id}`);
 
-});
-app.delete("/listings/:id",async(req,res)=>{
-    let{id}=req.params;
-    let deletedListing= await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
+
+
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
 });
 
+app.use ((err,req,res,next)=>{
+    let{statusCode=500, message="Something went wrong"}= err;
+    res.status(statusCode).render("error.ejs",{message})
+    // res.status(statusCode).send( message);
+   
+});
 
-// });
 app.listen(8080,()=>{
     console.log( "server is listening on port 8080");
 });
---
+++ Wonder Lust/routes/user.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+    res.render("users/signup.ejs");
+});
+
+router.post("/signup", wrapAsync(async (req, res) => {
+    try{
+        let { username, email, password } = req.body;
+
+        const newUser = new User({ email, username });
+        const registeredUser = await User.register(newUser, password);
+
+        console.log(registeredUser);
+        req.flash("success", "Welcome to the Staysphere!");
+        res.redirect("/listings");
+    }   catch(e){
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+ })
+);
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+
+router.post("/login", passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login"
+}),
+async(req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/listings");
+} 
+);    
+
+
+
+module.exports = router;
