56a2117ae6676b026175b41b7715ad7d7d32accd
Shivam Verma <inocentshivam2002@gmail.com>
2026-01-08 16:49:18 +0530

    Add working search feature and fixes

--- Wonder Lust/controllers/listings.js
+++ Wonder Lust/controllers/listings.js
@@ -4,10 +4,29 @@ const mapToken = process.env.MAP_TOKEN;
 const geoCodingClient = mbxGeocoding({ accessToken: mapToken });
 
 
-module.exports.index= async (req,res)=>{
-   const allListings= await Listing.find({});
-   res.render("listings/index.ejs",{allListings});
+module.exports.index = async (req, res) => {
+    const { search } = req.query;
+
+    let allListings;
+
+    if (search && search.trim() !== "") {
+        const regex = new RegExp(search, "i"); // case-insensitive
+
+        allListings = await Listing.find({
+            $or: [
+                { title: regex },
+                { location: regex },
+                { country: regex }
+            ]
+        });
+    } else {
+        allListings = await Listing.find({});
+    }
+
+    res.render("listings/index.ejs", { allListings });
 };
+
+
 
 
 module.exports.renderNewForm= (req,res)=>{
